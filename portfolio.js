14df834012661ec17566f52ae5ec2b541b933c0c
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-24 23:24:42 -0600

    Done a bit of work without pushing. Want to keep adding to this based on what I'm learning.

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+/*$('#link-buttons').hide();
+
+$('#toggle-links').click(function(){
+	$('#link-buttons').slideDown();
+	$('#toggle-links').hide();
+});
+
+$('#link-buttons').click(function(){
+	$('#link-buttons').slideUp();
+	$('#toggle-links').show();
+}); */
+
+
+
+
+
+
+
+
+
+
+}); //end jquery
