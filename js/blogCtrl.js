0a4aba57c17d4b2a63f88d56b0e6b370f1e0b811
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-27 19:40:59 -0600

    At least the blog is tied in now- got to fix the html

--- js/blogCtrl.js
+++ js/blogCtrl.js
@@ -1,7 +1,42 @@
 var app = angular.module('markT');
 
-app.controller('blogCtrl', function($scope) {
-		$scope.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
-		$scope.htmlcontent = $scope.orightml;
-		$scope.disabled = false;
-	});
+app.controller('blogCtrl', function($scope, blogService) {
+	
+	$scope.blogcontent = '';//need this, though the name can vary for TextAngular not to read undefined. 
+	$scope.htmlcontent = $scope.blogcontent;//send $scope.html to object called blog!
+	$scope.disabled = false; //this is for ng-show - but may not be important
+	
+
+$scope.submitBlog = function(){
+		
+		blogService.postParseBlog($scope.htmlcontent) //takes scope.blog and sends it on
+		
+		//we don't take any data in from the promise- because it just told us the promise ran
+		
+		//clear the blog content
+	}
+
+$scope.getBlog = function(){
+	
+		blogService.getParseBlog()
+		.then(function(blog){
+			//displayResults(blog);//comes back as an obj with an array of objects called results that is html code
+			$scope.blog = blog.results;
+		})
+	};
+	$scope.getBlog();
+
+});
+
+// app.controller('blogControl', function($scope, blogService){
+
+// 	$scope.blog = [{  //blog will need to be sent to parse service
+// 		title: $scope.blogtitle,
+// 		content: $scope.blogcontent
+// 	}]
+
+
+
+
+	
+// });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,12 +1,25 @@
 var app = angular.module('markT');
 
-app.controller('mainCtrl',function($scope){
+app.controller('mainCtrl', function($scope, blogService){
+
 $scope.test = "Hello World";
-	$scope.blog= [{
+
+	$scope.staticBlogTest= [{
 		title: "Mark's 1st Blog",   //needs to come dynamically
 		content: "This is where the dynamic content should come in"
-	}]
+	}];
 
 
+	//I need to figure how to allow my index.html to access this controller- earlier attempts didn't work . . .
+	$scope.getBlog = function(){
+	
+		blogService.getParseBlog()
+		.then(function(blog){
+			//displayResults(blog);//comes back as an obj with an array of objects called results that is html code
+			
+			$scope.blog = blog.results;//display all blogs- could limit
+		})
+	};
+	$scope.getBlog();
 
 });
